9d29f5500c3b1e14638b0ef39485cd8ad4997251
alexvgz <72766595+alexvgz@users.noreply.github.com>
2022-03-06 20:48:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 window.onload = choosePic;
 
-var myLogo = ["look/banner/banner0.png", "look/banner/IDASTouhou_Title_Logo.png"];
+var myLogo = ["look/banner/banner0.png", "look/banner/IDASTouhou_TitleLogo.png"];
 
 function choosePic() {
 
