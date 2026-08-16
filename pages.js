7e12755e03d8cf309eb5cda3deb7767f8f42ae03
alexvgz <72766595+alexvgz@users.noreply.github.com>
2026-08-16 02:56:58 -0700

    making pages.js independent of folder placement

--- pages.js
+++ pages.js
@@ -1,3 +1,5 @@
+const siteRoot = new URL(".", document.currentScript.src);
+
 var myLogo = [
     "look/banner/banner0.png",
     "look/banner/IDASTouhou3.png",
@@ -5,33 +7,31 @@ var myLogo = [
     "look/banner/TouhouInfinity.png"
 ];
 
-
 function choosePic() {
     var logo = document.getElementById("logoindex");
 
     if (!logo) return;
 
     var randomNum = Math.floor(Math.random() * myLogo.length);
-    logo.src = myLogo[randomNum];
+    logo.src = new URL(myLogo[randomNum], siteRoot).href;
 }
 
 
 const imagesToPreload = [
-    "/look/buttons/home2.png",
-    "/look/buttons/game2.png",
-    "/look/buttons/consoles2.png",
-    "/look/buttons/anime2.png",
-    "/look/buttons/cartoons2.png",
-    "/look/buttons/comic2.png",
-    "/look/buttons/info2.png",
-    "/look/buttons/projects2.png",
-    "/look/buttons/collection2.png"
+    "look/buttons/home2.png",
+    "look/buttons/game2.png",
+    "look/buttons/consoles2.png",
+    "look/buttons/anime2.png",
+    "look/buttons/cartoons2.png",
+    "look/buttons/comic2.png",
+    "look/buttons/info2.png",
+    "look/buttons/projects2.png",
+    "look/buttons/collection2.png"
 ];
-
 
 imagesToPreload.forEach(src => {
     const img = new Image();
-    img.src = src;
+    img.src = new URL(src, siteRoot).href;
 });
 
 
