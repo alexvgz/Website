1f157a6d22333e01bf084ee92449d6878296fe80
alexvgz <72766595+alexvgz@users.noreply.github.com>
2026-08-16 02:16:45 -0700

    added preload images to the script

--- pages.js
+++ pages.js
@@ -9,3 +9,20 @@ function choosePic() {
 
  
  }
+
+const imagesToPreload = [
+    "/look/buttons/home2.png",
+    "/look/buttons/game2.png",
+    "/look/buttons/consoles2.png",
+    "/look/buttons/anime2.png",
+    "/look/buttons/cartoons2.png",
+    "/look/buttons/comic2.png",
+    "/look/buttons/info2.png",
+    "/look/buttons/projects2.png",
+    "/look/buttons/collection2.png"
+];
+
+imagesToPreload.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
